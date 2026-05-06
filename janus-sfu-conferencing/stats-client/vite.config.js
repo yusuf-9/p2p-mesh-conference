2e8da0fc76fbd9cfd23e88065851c5498d8ae60f
yusuf-ts <yusuf@tericsoft.com>
2026-05-07 00:55:53 +0530

    fix (stat-client): change base url

--- janus-sfu-conferencing/stats-client/vite.config.js
+++ janus-sfu-conferencing/stats-client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/client/',
+  base: '/stats/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
